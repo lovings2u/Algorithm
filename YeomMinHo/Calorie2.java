b1f2b0102ad6d110b1337f54a2538c9e2b2c6254
yumms01 <yumms01@nate.com>
2016-05-04 12:42:24 +0900

    ss
    ss

--- YeomMinHo/Calorie2.java
+++ YeomMinHo/Calorie2.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Calorie2 {
 	public static void main (String[] args){
 		Scanner sc = new Scanner(System.in);
-		int bugger[] = {0,461,431,420,0};
-		int drink[] = {0,130,160,118,0};
-		int side[] = {0,100,57,70,0};
-		int dessert[] = {0,167,266,75,0};
+		int bugger[] = {461,431,420,0};
+		int drink[] = {130,160,118,0};
+		int side[] = {100,57,70,0};
+		int dessert[] = {167,266,75,0};
 		int a,b,c,d;
 		int calorie=0;
 		
@@ -15,7 +15,7 @@ public class Calorie2 {
 		c = sc.nextInt();
 		d = sc.nextInt();
 		
-		calorie = bugger[a] + drink[b] + side[c] + dessert[d];
+		calorie = bugger[a+1] + drink[b+1] + side[c+1] + dessert[d+1];
 		System.out.println("Your total Calorie count is " + calorie + ".");
 	}
 }
