71a696f9ab74f341f4bd8a89985eb08fd8900a48
byungeungil <gbe1205@naver.com>
2016-05-10 19:41:49 +0900

    Add files via upload

--- GilByeongEun/even_odd2.java
+++ GilByeongEun/even_odd2.java
@@ -6,8 +6,8 @@ public class Main {
         int si = sc.nextInt();
         String fib = fi%2==0 ?  "even" : "odd";
         String sib = si%2==0 ?  "even" : "odd";
-        String rib = (fi+si)%2==0 ? "even" : "odd";
-        String rib2 = (fi*si)%2==0 ? "even" : "odd";
+        String rib = fib.length()==sib.length() ? "even" : "odd";
+        String rib2 = fib.length()== 4 || sib.length() ==4 ? "even" : "odd";
         System.out.println(fib+"+"+sib+"="+rib);
         System.out.println(fib+"*"+sib+"="+rib2);
         sc.close();
