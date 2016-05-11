dd712d93a5930016011fe274dab27b9646d82eff
minzy626 <mjalswl626@gmail.com>
2016-05-11 17:12:12 +0900

    Update queen.java

--- KimMinJi/queen.java
+++ KimMinJi/queen.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
 public class queen {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n,m;
+		n=sc.nextLong();
+		m=sc.nextLong();
+		if(n!=m){
+			System.out.println(n>m? m: n);
+		}else {
+			System.out.println(n);
 
+		}
+	
 }
+}
+
