b26a4384969f928fdb3bcf5361057a18e76620fa
minzy626 <mjalswl626@gmail.com>
2016-05-15 06:11:18 +0900

    Update clo.java

--- KimMinJi/clo.java
+++ KimMinJi/clo.java
@@ -7,17 +7,13 @@ public class clo {
 		int h=sc.nextInt();
 		double m;
 		
-		//입력한 시간의 시침위치 (360/12)*h
-		//븐침이 1분에 360/60씩감  1분에 6도씩 이동
-		//시침은 1시간에 30도움직임
-		//그럼 시침은 1분에 60/30 0.5도 움직임
-		//분침6m이랑 시침30h+1/2m이 같아야함
+	
 		
-		m=(60/11)*h;
+		m=(double)(60/11)*h;
 		
 		if(h==12){System.out.println("0.000000");}
 		else{
-			if(m<5*h){
+			if(m<5*h+5){
 				System.out.printf("%.6h",m);
 			}else{
 				System.out.println("CAN'T SEE!");}
