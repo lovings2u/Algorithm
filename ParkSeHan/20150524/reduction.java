b9bc43c8b347d3f0eb60f27dfc365bfeb2d983ca
sh1003 <sehan1003@naver.com>
2016-05-24 16:51:58 +0900

    sehan
    sehan

---
+++ ParkSeHan/20150524/reduction.java
@@ -0,0 +1,37 @@
+package com.hybrid.exam;
+
+import java.util.Scanner;
+
+public class reduction {
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int c = sc.nextInt(), d = sc.nextInt();
+        int x = 0, y = 0; 
+        int z = 0, w = 0;
+        if(a>b){
+            x = a;
+            y = b;
+        } else {
+            x = b;
+            y = a;
+        }
+        if(c>d){
+            z = c;
+            w = d;
+        } else {
+            z = d;
+            w = c;
+        }
+        double dx = (double) z/x;
+        double dy = (double) w/y;     
+        if(x<z && y<w){
+            System.out.println("100%");
+        }else if(dx > dy){
+            System.out.println((int)(dy*100) +"%");
+        }else{
+            System.out.println((int)(dx*100) +"%");
+        }
+    }
+}
