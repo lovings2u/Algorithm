6eafb1373c3b250d6376856fc8615d572b9c293d
minzy626 <mjalswl626@gmail.com>
2016-06-21 18:23:29 +0900

    Update beat.java

--- KimMinJi/beat.java
+++ KimMinJi/beat.java
@@ -1,23 +1,21 @@
-package java0511;
-
 import java.util.Scanner;
-
-public class beat {
-
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		int s=sc.nextInt();
-		int d=sc.nextInt();
-		int a,b;
-		
-		if(s<d){
-			System.out.println("impossible");
-		}else{
-			a=(s+d)/2;
-			b=(s-d)/2;
-			if((s+d)%2!=0){System.out.println("impossible");}
-			else{System.out.println(a+" "+b);}
-		}
-	}
-
-}
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int s=sc.nextInt();
+        int d=sc.nextInt();
+        int a,b;
+         
+        if(s<d){
+            System.out.println("impossible");
+        }else{
+            a=(s+d)/2;
+            b=(s-d)/2;
+	    if(a==0&&b==0){System.out.println(a+" "+b);}
+            else if((s+d)%2!=0){System.out.println("impossible");}
+                         
+            else{System.out.println(a+" "+b);}
+        }
+    }}
