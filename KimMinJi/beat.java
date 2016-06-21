c28b25103b8e86d542ca433733c63d0d80a24f92
minzy626 <mjalswl626@gmail.com>
2016-06-21 18:27:45 +0900

    Update beat.java

--- KimMinJi/beat.java
+++ KimMinJi/beat.java
@@ -14,7 +14,7 @@ public class Main {
             a=(s+d)/2;
             b=(s-d)/2;
 	    if(a==0&&b==0){System.out.println(a+" "+b);}
-            else if((s+d)%2!=0){System.out.println("impossible");}
+            else if((s+d)%2!=0){System.out.println("impossible");}//s는 a+b인거고 d는 a-b이기때문에 s+d는 2a이라서 짝수.따라서 홀수면 impossible
                          
             else{System.out.println(a+" "+b);}
         }
