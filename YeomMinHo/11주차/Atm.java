dc39ab1d030f98d618600910ff00460be1d754b4
yumms01 <yumms01@nate.com>
2016-07-12 10:47:44 +0900

    수정
    수정

--- YeomMinHo/11주차/Atm.java
+++ YeomMinHo/11주차/Atm.java
@@ -1,3 +1,4 @@
+﻿import java.util.Arrays;
 import java.util.Scanner;
 
 public class Atm {
@@ -13,7 +14,8 @@ public class Atm {
 			arr[i] = sc.nextInt();
 		}
 		
-		arr = bubbleSort(arr);
+//		arr = bubbleSort(arr);
+		Arrays.sort(arr);
 		
 		for (int i=0 ; i<arr.length ; i++) {
 			temp += arr[i];
@@ -22,19 +24,19 @@ public class Atm {
 		System.out.println(sum);
 	}
 	
-	public static int[] bubbleSort(int[] arr) {
-		for(int i=0; i<arr.length-1; i++) {             // 10개의 값이 있으면 두 개의 값을 비교하는 과정은 9번만 진행한다   
-			for(int j=0; j<(arr.length-i)-1; j++) {     //가장 큰 값을 뒤로 보내면 마지막 값 고정이되는데 i++만큼 증가하므로 -i 해준다
-			
-				if(arr[j] > arr[j+1])        //가장 큰값을 뒤로 보내준다
-				{
-					int tmp = arr[j+1];
-					arr[j+1] = arr[j];
-					arr[j] = tmp;
-				}	
-			}
-		}
-		
-		return arr;
-	}
+//	public static int[] bubbleSort(int[] arr) {
+//		for(int i=0; i<arr.length-1; i++) {             // 10개의 값이 있으면 두 개의 값을 비교하는 과정은 9번만 진행한다   
+//			for(int j=0; j<(arr.length-i)-1; j++) {     //가장 큰 값을 뒤로 보내면 마지막 값 고정이되는데 i++만큼 증가하므로 -i 해준다
+//			
+//				if(arr[j] > arr[j+1])        //가장 큰값을 뒤로 보내준다
+//				{
+//					int tmp = arr[j+1];
+//					arr[j+1] = arr[j];
+//					arr[j] = tmp;
+//				}	
+//			}
+//		}
+//		
+//		return arr;
+//	}
 }
