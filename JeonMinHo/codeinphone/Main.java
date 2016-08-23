e5e9f50d6d734b9b58af1cac7a0b2a52f5c2aeab
lovings2u <skyhan1106@naver.com>
2016-08-23 10:32:56 +0900

    20160823

--- JeonMinHo/codeinphone/Main.java
+++ JeonMinHo/codeinphone/Main.java
@@ -11,12 +11,20 @@ public class Main {
 		int code = sc.nextInt();
 		int select = sc.nextInt()-1;
 		String[] inputArr = numString.split("-");
-		if(inputArr[select].length() > 4) {
-			System.out.println("INPUT ERROR!");
-			return;
+		for(int i = 0; i < inputArr.length; i++) {
+			if(inputArr[i].length() > 4) {
+				System.out.println("INPUT ERROR!");
+				return;
+			}
 		}
-		String input = String.format("%4s", inputArr[select]);
-
+		String input = "";
+		System.out.println(inputArr.length);
+		if(inputArr.length <= select) {
+			input = "    ";
+		} else {
+			input = String.format("%4s", inputArr[select]);
+		}
+		
 		String[] temp = input.split("");
 		StringBuilder result = new StringBuilder();
 		for(int i = 0; i < 4; i++) {
