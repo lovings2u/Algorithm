f3adcd4aec0fe968f6f56df071452081b9554ef0
djsc6511 <djsc6511@naver.com>
2017-01-20 14:58:22 +0900

    11656

--- KwonSoonGu/Q11656.java
+++ KwonSoonGu/Q11656.java
@@ -1,7 +1,28 @@
 package KwonSoonGu;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 /**
  * Created by Soongu on 2017-01-20.
  */
 public class Q11656 {
+    public static void main(String[] args) {
+        String s;
+        ArrayList<String> words = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        s = input.nextLine();
+
+        for (int i=0;i<s.length();i++) {
+            words.add(s.substring(i,s.length()));
+        }
+
+        Collections.sort(words);
+
+        for (int i=0;i<words.size();i++) {
+            System.out.println(words.get(i));
+        }
+    }
 }
