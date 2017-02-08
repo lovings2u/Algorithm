92fecdcbebf85af6621f5c8f92a335c6d954e77b
clcell3 <clcell3@naver.com>
2017-02-08 19:30:17 +0900

    Add files via upload

---
+++ JungJiHwan/170208/Q1024.java
@@ -0,0 +1,73 @@
+package JungOl;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Pos{
+	int x, y;
+	public Pos(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class Q1024 {
+	ArrayList<Pos> pos;
+	int front = 0;
+	int rear = -1;
+	public static void main(String[] args) {new Q1024();}
+	public Q1024() {
+		Scanner sc = new Scanner(System.in);
+		pos = new ArrayList<>();
+		int count = 0, a = sc.nextInt(), b = sc.nextInt();
+		int mat[][] = new int[a][b];
+		
+		for(int i = 0; i<a; i++){
+			for(int j = 0; j<b; j++){
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		sc.close();
+		
+		push(new Pos(0, 0));
+		
+		while(true){
+			Pos p = pop();
+			if(p == null) break;
+			if(p.x != a - 1){
+				if(mat[p.x + 1][p.y] < mat[p.x][p.y]){
+					push(new Pos(p.x + 1, p.y));
+				}
+			}
+			if(p.y != b - 1){
+				if(mat[p.x][p.y + 1] < mat[p.x][p.y]){
+					push(new Pos(p.x, p.y + 1));
+				}
+			}
+			if(p.x != 0){
+				if(mat[p.x - 1][p.y] < mat[p.x][p.y]){
+					push(new Pos(p.x - 1, p.y));
+				}
+			}
+			if(p.y != 0){
+				if(mat[p.x][p.y - 1] < mat[p.x][p.y]){
+					push(new Pos(p.x, p.y - 1));
+				}
+			}
+			if(p.y == b - 1 && p.x == a - 1){
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+	
+	void push(Pos pos){
+		++rear;
+		this.pos.add(pos);
+	}
+	
+	Pos pop(){
+		if(front>rear) return null;
+		return pos.get(front++);
+	}
+}
